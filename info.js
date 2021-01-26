a9d83aac21e85c695c58313d9f012dd0ea026dbf
weiseng18 <tws.hci@gmail.com>
2021-01-26 10:53:46 +0800

    [info] create info.js, data amalgamation widget; currently includes only active coronavirus cases

---
+++ info.js
@@ -0,0 +1,51 @@
+/*
+	Description:
+	This widget aims to fill the home screen with lots of data, that might not necessarily be useful, but just good to know.
+	1. Gets coronavirus numbers from https://coronavirus-19-api.herokuapp.com/countries/singapore
+
+	This widget's text is designed to be large, top, with text right aligned.
+*/
+
+const contentFont = Font.semiboldSystemFont(16);
+
+const include_coronavirus = true;
+
+function getBackgroundImage(type) {
+	// backgroundImage
+	let FM = FileManager.local();
+	let path = FM.bookmarkedPath("Images");
+
+	// e.g. small_middle_left
+	// depends on the widget's parameter
+	return FM.readImage(path + "/" + type + ".png");
+}
+
+async function run() {
+	let widget = new ListWidget();
+	widget.backgroundImage = getBackgroundImage(args.widgetParameter);
+
+	// adds active coronavirus cases
+	if (include_coronavirus) {
+		const url = "https://coronavirus-19-api.herokuapp.com/countries/singapore";
+		const req = new Request(url);
+		const data = await req.loadJSON();
+
+		let description = widget.addText("Active cases:");
+		description.rightAlignText();
+		description.textColor = Color.white();
+		description.font = contentFont;
+		widget.addSpacer(5);
+
+		let amount = widget.addText(data["active"].toString());
+		amount.rightAlignText();
+		amount.textColor = Color.white();
+		amount.font = contentFont;
+	}
+	
+	widget.addSpacer();
+
+	Script.setWidget(widget);
+	Script.complete();
+}
+
+await run();
