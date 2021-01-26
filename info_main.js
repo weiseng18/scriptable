ae6f83711206827a514d509feb01b6052c4c79de
weiseng18 <tws.hci@gmail.com>
2021-01-26 18:40:01 +0800

    [info_main] add battery info

--- info_main.js
+++ info_main.js
@@ -6,6 +6,8 @@
 const headerFont = Font.boldSystemFont(20);
 const contentFont = Font.semiboldSystemFont(16);
 
+let include_battery = true;
+
 function getBackgroundImage(type) {
 	// backgroundImage
 	let FM = FileManager.local();
@@ -23,6 +25,22 @@ async function run() {
 	widget.backgroundImage = getBackgroundImage(imageSrc);
 	let colorType = param[1];
 	let color = colorType == "white" ? Color.white() : Color.black();
+
+	if (include_battery) {
+		let description = widget.addText("Battery");
+		description.rightAlignText();
+		description.textColor = color;
+		description.font = headerFont;
+		widget.addSpacer(5);
+
+		let batteryLevel = Math.round(Device.batteryLevel()*100);
+    	let batteryText = batteryLevel.toString() + "%";
+		let battery = widget.addText(batteryText);
+		battery.rightAlignText();
+		battery.textColor = color;
+		battery.font = contentFont;
+		widget.addSpacer(5);
+	}
 
 	widget.addSpacer();
 
